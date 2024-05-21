841cc51f951b6ee7f1a973146551dd53dfc3668f
Bahar Gümüşer <121288876+bahargumuser@users.noreply.github.com>
2024-05-21 11:32:19 +0300

    add order header

---
+++ src/components/Orderpizza.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Orderpizza() {
+    return (
+        <header>
+            <div className='div-order'>
+                <h1>Teknolojik Yemekler</h1>
+                <p>Anasayfa - <span className='order'>Sipariş Oluştur</span></p>
+            </div>
+        </header>
+    )
+}
